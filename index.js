ca4c3940c4c04f5db662a548a49fa6d12df60639
Markku Vaara <markku.vaara45@gmail.com>
2023-07-01 15:56:07 +0300

    Lisätty json-parseri ja välttämättömät middlewaret

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const app = express();
 
+let images = [
+    1000, 1001, 1002
+]
+
+app.use(express.json());
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method);
+    console.log('Path:  ', request.path);
+    console.log('Body:  ', request.body);
+    console.log('---');
+    next();
+};
+
+app.use(requestLogger);
+
 app.get('/', (req, res) => {
     res.send('<h1>Simple muistipeli server</h1>')
 });
 
-const PORT = 3001
+app.get('/images', (req, res) => {
+    res.json(images);
+});
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+};
+  
+app.use(unknownEndpoint);
+
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 });
