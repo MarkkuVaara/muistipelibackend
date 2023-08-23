488a3f54fe6467ddfa25a4e5bcb4f50c81999566
Markku Vaara <markku.vaara45@gmail.com>
2023-08-23 22:29:49 +0300

    Added image transfer limit

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ mongoose.connect(config.MONGODB_URI)
   })
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '1mb'}));
+app.use(express.urlencoded({limit: '1mb'}));
 
 app.use(middleware.requestLogger);
 
